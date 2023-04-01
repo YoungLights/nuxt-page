959a831fa11d6b69f38d8ca4e4371149891f2bcf
Young Lights <hello@younglights.de>
2023-04-02 00:02:45 +0200

    Adding Styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,16 @@
 	<Head>
 		<Title>Nuxt Website</Title>
 	</Head>
-	<nav>
-		<NuxtLink to="/">Index</NuxtLink>
-		<NuxtLink to="/about">About</NuxtLink>
-		<NuxtLink to="/services">Services</NuxtLink>
-		<NuxtLink to="/buhu">Buhu</NuxtLink>
+	<nav class="nav">
+		<div class="nav-logo">
+			<h4>🔥 NUXT WEBSITE</h4>
+		</div>
+		<div class="nav-links">
+			<NuxtLink to="/">Index</NuxtLink>
+			<NuxtLink to="/about">About</NuxtLink>
+			<NuxtLink to="/services">Services</NuxtLink>
+			<NuxtLink to="/buhu">Buhu</NuxtLink>
+		</div>
 	</nav>
 	<slot />
 </template>

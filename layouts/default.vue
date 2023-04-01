c3c9e431817cf6de8888188ee32dc51a5d14bb31
Young Lights <hello@younglights.de>
2023-04-01 22:56:08 +0200

    new pages Buhu

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
 		<NuxtLink to="/">Index</NuxtLink>
 		<NuxtLink to="/about">About</NuxtLink>
 		<NuxtLink to="/services">Services</NuxtLink>
+		<NuxtLink to="/buhu">Buhu</NuxtLink>
 	</nav>
 	<slot />
 </template>
---
+++ pages/buhu.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Buhu</h1>
+</template>

c7708be95f108c263ecfe9c03986e47b69ba99f3
Young Lights <hello@younglights.de>
2023-04-03 23:00:17 +0200

    New styles

---
+++ components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+	<nav class="nav">
+		<div class="nav-logo">
+			<h4>🔥 NUXT WEBSITE</h4>
+		</div>
+		<div class="nav-links">
+			<NuxtLink to="/">Index</NuxtLink>
+			<NuxtLink to="/about">About</NuxtLink>
+			<NuxtLink to="/services">Services</NuxtLink>
+			<NuxtLink to="/buhu">Buhu</NuxtLink>
+		</div>
+	</nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,8 @@
 	<Head>
 		<Title>Nuxt Website</Title>
 	</Head>
-	<nav class="nav">
-		<div class="nav-logo">
-			<h4>🔥 NUXT WEBSITE</h4>
-		</div>
-		<div class="nav-links">
-			<NuxtLink to="/">Index</NuxtLink>
-			<NuxtLink to="/about">About</NuxtLink>
-			<NuxtLink to="/services">Services</NuxtLink>
-			<NuxtLink to="/buhu">Buhu</NuxtLink>
-		</div>
-	</nav>
-	<slot />
+	<Nav />
+	<div class="frame">
+		<slot />
+	</div>
 </template>
